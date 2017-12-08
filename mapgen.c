80af8ea1f4aad2273090b81deedf1314d2556d7c
GaoningGao <33690279+GaoningGao@users.noreply.github.com>
2017-12-08 23:39:59 +0100

    Update mapgen.c

--- mapgen.c
+++ mapgen.c
@@ -1,1 +1,26 @@
 #include "mapgen.h"
+#define SIZE 20
+
+
+int random_number(int min, int max){
+
+    return (rand()%(max-min+1)+min);
+
+}
+
+void generate_map(char map[100][100],int min,int max){
+    int i,j;
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<SIZE;j++){
+            map[i][j]=random_number(min,max);
+            if(map[i][j]==0)
+                printf("   ");
+            else printf(" %c ",map[i][j]);
+        }
+    
+    
+    }
+
+
+
+}
